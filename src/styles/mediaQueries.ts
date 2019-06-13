b3d2097c3c3665f286ec1025af1313921c49eb9b
ES <es019@protonmail.com>
2019-06-13 17:28:26 +0700

    Add mediaQueries.ts

--- src/styles/mediaQueries.ts
+++ src/styles/mediaQueries.ts
@@ -1,17 +1,28 @@
+export const breakpoints = {
+  xs: 0,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+};
+
 export const mq = {
   getStr(size: number) {
     return `@media (min-width: ${size}px)`;
   },
+  get xs() {
+    return this.getStr(breakpoints.xs);
+  },
   get sm() {
-    return this.getStr(576);
+    return this.getStr(breakpoints.sm);
   },
   get md() {
-    return this.getStr(768);
+    return this.getStr(breakpoints.md);
   },
   get lg() {
-    return this.getStr(992);
+    return this.getStr(breakpoints.lg);
   },
   get xl() {
-    return this.getStr(1200);
+    return this.getStr(breakpoints.xl);
   },
 };
