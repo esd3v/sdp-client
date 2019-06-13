1d715187e5e33232b228cdd046e3a4e7f82d7d6f
ES <es019@protonmail.com>
2019-06-13 17:28:44 +0700

    Update webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
           {
             loader: 'tslint-loader',
             query: {
-              failOnHint: true,
+              failOnHint: false,
             },
           },
         ],
