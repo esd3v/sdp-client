b019f64515f746de4a727c061891fb8ec7960986
ES <es019@protonmail.com>
2019-06-13 17:42:24 +0700

    Make searchbar responsive.

--- src/components/searchBar/styles.ts
+++ src/components/searchBar/styles.ts
@@ -1,8 +1,22 @@
 import {css} from 'emotion';
 import * as variables from 'styles/variables';
+import {mq} from 'styles/mediaQueries';
 
 export const searchBar = css`
   display: flex;
+  flex-direction: column;
   margin-bottom: 12px;
-  box-shadow: ${variables.shadow};
+  & > *:first-of-type {
+    margin-bottom: 16px;
+    box-shadow: ${variables.shadow};
+  }
+  button {
+    min-height: 36px;
+  }
+  ${mq.sm} {
+    flex-direction: row;
+    & > *:first-of-type {
+      margin-bottom: 0;
+    }
+  }
 `;
