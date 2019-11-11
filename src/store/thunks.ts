6e04aa777edaa9f13375c4c919a07767bed92744
ES <es019@protonmail.com>
2019-11-12 02:13:14 +0700

    Update thunks.

--- src/store/thunks.ts
+++ src/store/thunks.ts
@@ -4,27 +4,35 @@ import {actions} from 'store/actions';
 export const loadTopics: Thunks['loadTopics'] = ({appID, page, perPage}) =>
   async (dispatch, getState) => {
     const {global: {loading}} = getState();
+
     if (!loading) {
       dispatch(actions.global.setLoading(true));
+      dispatch(actions.global.setStatus({
+        message: '',
+        type: 'normal',
+      }));
+
       try {
         const [error, result] = await api.loadTopics({appID, page, perPage});
 
         if (error) {
-          const statusMessage = error.response ?
-            error.response.data.message :
-              `Couldn't connect to the server`;
-
-          dispatch(actions.global.setStatus({
-            message: statusMessage,
-            type: 'error',
-          }));
+          if (error.response) {
+            throw new Error(error.response.data.message);
+          } else {
+            throw new Error(`Couldn't connect to the server`);
+          }
         }
+
         dispatch(actions.parser.setTopics(result.data.topics));
         dispatch(actions.parser.setPageTotal(result.data.pageTotal));
         dispatch(actions.parser.setTopicTotal(result.data.topicTotal));
         dispatch(actions.global.setLoading(false));
-      } catch {
+      } catch (err) {
         dispatch(actions.global.setLoading(false));
+        dispatch(actions.global.setStatus({
+          message: err.message,
+          type: 'error',
+        }));
       }
     }
   };
