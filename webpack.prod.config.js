eed0c463c099fa48d1cb9e3cb63be19c690bfa1a
ES <es019@protonmail.com>
2019-11-12 02:14:46 +0700

    Update webpack config.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,10 +1,8 @@
-const path = require('path');
 const merge = require('webpack-merge');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const baseConfig = require('./webpack.config.js');
-const DIR_BUILD = path.resolve(__dirname, './dist');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
@@ -12,7 +10,7 @@ module.exports = merge(baseConfig, {
     splitChunks: {
       name: 'vendor',
       chunks: 'all',
-    }
+    },
   },
   plugins: [
     new TerserPlugin(),
