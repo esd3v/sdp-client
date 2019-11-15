a22c100452c6565dd495f40632df6e2756d5f322
ES <es019@protonmail.com>
2019-11-16 01:04:28 +0700

    Tweak Main styles.

--- src/components/main/styles.ts
+++ src/components/main/styles.ts
@@ -2,9 +2,10 @@ import {css} from 'emotion';
 import {mq} from 'styles/mediaQueries';
 
 export const main = css`
-  padding: 32px;
+  padding: 12px;
   margin: 0 auto;
   ${mq.sm} {
     width: 576px;
+    padding: 32px;
   }
 `;
