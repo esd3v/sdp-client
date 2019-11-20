6dfc38da776fb5d7e5aa1b30a00c1f35c27a7da2
ES <es019@protonmail.com>
2019-11-20 23:03:31 +0700

    Update fonts, global styles.

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,27 +1,24 @@
 import {injectGlobal} from 'emotion';
-import 'assets/fonts/Roboto-Regular.woff';
-import 'assets/fonts/Roboto-Regular.woff2';
-import 'assets/fonts/Roboto-Medium.woff';
-import 'assets/fonts/Roboto-Medium.woff2';
+import RobotoRegularWoff from 'assets/fonts/Roboto-Regular.woff';
+import RobotoRegularWoff2 from 'assets/fonts/Roboto-Regular.woff2';
+import RobotoMediumWoff from 'assets/fonts/Roboto-Medium.woff';
+import RobotoMediumWoff2 from 'assets/fonts/Roboto-Medium.woff2';
 
-injectGlobal({
-  '@font-face': {
-    fontFamily: 'Roboto',
-    src: `
-      url('/assets/fonts/Roboto-Regular.woff2') format('woff2'),
-      url('/assets/fonts/Roboto-Regular.woff') format('woff')
-    `,
-    fontWeight: 'normal',
-    fontStyle: 'normal',
+injectGlobal`
+  @font-face {
+    font-family: 'Roboto';
+    font-weight: 'normal';
+    font-style: 'normal';
+    src:
+      url('${RobotoRegularWoff2}') format('woff2'),
+      url('${RobotoRegularWoff}') format('woff');
+  }
+  @font-face {
+    font-family: 'Roboto';
+    font-weight: 500;
+    font-style: normal;
+    src:
+      url('${RobotoMediumWoff2}') format('woff2'),
+      url('${RobotoMediumWoff}') format('woff');
   },
-}, {
-  '@font-face': {
-    fontFamily: 'Roboto',
-    src: `
-      url('/assets/fonts/Roboto-Medium.woff2') format('woff2'),
-      url('/assets/fonts/Roboto-Medium.woff') format('woff')
-    `,
-    fontWeight: 500,
-    fontStyle: 'normal',
-  },
-});
+`;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,27 +1,31 @@
 import {injectGlobal} from 'emotion';
 
-injectGlobal({
-  html: {
-    height: '100%',
-    fontSize: '16px',
-    boxSizing: 'border-box',
-  },
-  '*, *:before, *:after': {
-    boxSizing: 'inherit',
-  },
-  body: {
-    margin: 0,
-    padding: 0,
-    fontFamily: 'Roboto, Arial, sans-serif',
-    color: '#333',
-    fontSize: '16px',
-    backgroundColor: '#eee',
-  },
-  'input, button': {
-    border: 0,
-    outline: 0,
-  },
-  '.tippy-backdrop': {
-    backgroundColor: 'rgba(51, 51, 51, 0.85)',
-  },
-});
+injectGlobal`
+  html {
+    height: 100%;
+    font-size: 16px;
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Roboto, Arial, sans-serif;
+    color: #333;
+    font-size: 16px;
+    background-color: #e5e5e5;
+  }
+  input, button {
+    border: 0;
+    outline: 0;
+  }
+  button, input, select, textarea {
+    font-family: inherit;
+    font-size: 100%;
+  }
+  .tippy-backdrop {
+    background-color: rgba(51, 51, 51, 0.85);
+  }
+`;
