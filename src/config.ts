216309fd10b2babfc0f12676c9467a9b04124169
ES <es019@protonmail.com>
2019-11-20 22:55:40 +0700

    Update config.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,5 @@
-export const API_LOCALPORT = 8080;
-// export const API_ENDPOINT = `https://sdp-server.herokuapp.com`;
-// export const API_WS_ENDPOINT = `wss://sdp-server.herokuapp.com`;
-export const API_ENDPOINT = `http://localhost:${API_LOCALPORT}`;
-export const API_WS_ENDPOINT = `ws://localhost:${API_LOCALPORT}`;
+export const API_PORT = 8080;
+export const API_ENDPOINT = `http://localhost:${API_PORT}`;
+export const API_WS_ENDPOINT = `ws://localhost:${API_PORT}`;
 export const API_TIMEOUT = 1.5 * 60 * 1000;
 export const PERPAGE: PerPage[] = [15, 30, 50];
