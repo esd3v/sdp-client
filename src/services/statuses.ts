b35e0f54a2fa6ce476b2a34993a5064669f12578
ES <es019@protonmail.com>
2019-12-23 02:48:26 +0700

    Add statuses for ws events

--- src/services/statuses.ts
+++ src/services/statuses.ts
@@ -12,22 +12,32 @@ export const createNormalStatus = (message: string) =>
     type: 'normal',
   });
 
-export const empty = createStatus({
-  message: '',
-  type: 'normal',
-});
+export const createSuccessStatus = (message: string) =>
+  createStatus({
+    message,
+    type: 'success',
+  });
 
-export const couldntConnectToTheServer = createStatus({
-  message: `Couldn't connect to the server`,
-  type: 'error',
-});
+export const empty =
+  createNormalStatus('');
 
-export const appIDIsNotAnInteger = createStatus({
-  message: `'appID' must be an integer`,
-  type: 'error',
-});
+export const couldntConnectToTheServer =
+  createErrorStatus(`Couldn't connect to the server`);
 
-export const pageIsNotAnInteger = createStatus({
-  message: `'page' must be an integer`,
-  type: 'error',
-});
+export const appIDIsNotAnInteger =
+  createErrorStatus(`'appID' must be an integer`);
+
+export const pageIsNotAnInteger =
+  createErrorStatus(`'page' must be an integer`);
+
+export const websocketHasBeenLost =
+  createErrorStatus('WebSocket connection has been lost. Reconnecting...');
+
+export const websocketHasBeenOpened =
+  createNormalStatus('WebSocket connection has been opened.');
+
+export const websocketHasBeenRestored =
+  createSuccessStatus('WebSocket connection has been restored.');
+
+export const websocketUnableToConnect =
+  createErrorStatus('Unable to connect to WebSocket server.');
