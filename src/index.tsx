c89f9b38cbff1d0d237034d3dfa2afbcef6404a9
ES <es019@protonmail.com>
2019-12-23 02:23:55 +0700

    Generate sessionID on startup

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import cryptoRandomString from 'crypto-random-string';
 import * as React from 'react';
 import {createBrowserHistory} from 'history';
 import {render} from 'react-dom';
@@ -7,6 +8,10 @@ import {Root} from 'components/routes/root';
 import {ErrorBoundary} from 'components/errorBoundary';
 require('./styles/global');
 require('./styles/fonts');
+
+if (!sessionStorage.getItem('sessionID')) {
+  sessionStorage.setItem('sessionID', cryptoRandomString({length: 16}));
+}
 
 const history = createBrowserHistory();
 
